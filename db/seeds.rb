348a2dc8a9ad1aaf4a75bb519b291b0e9fbde283
Simon <simonireilly@gmail.com>
2018-12-29 11:09:26 +0000

    Setup an article scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |number|
+  Article.new.tap do |article|
+    article.title = "Blog post #{number}"
+    article.body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae justo eu massa imperdiet commodo eget sed mauris. Vestibulum leo mauris, vulputate et elit quis, laoreet fringilla turpis. Donec posuere ligula nec lectus euismod, vitae ullamcorper velit placerat. Integer urna risus, euismod et tristique a, tempus vitae ligula. Pellentesque ut imperdiet lectus. Donec at eros vitae nisi dictum ultrices nec ac odio. Nam vulputate justo mi, eu aliquet mi sagittis eget. Phasellus sed erat eget augue consequat porttitor. Morbi sit amet aliquet turpis. Ut semper porttitor leo, nec mattis magna eleifend at. Nullam rhoncus erat nec molestie ornare. Maecenas a arcu bibendum, gravida felis at, iaculis massa. Sed posuere at dolor vel auctor. Mauris ac arcu lacinia, vulputate diam sit amet, mattis turpis. Nullam dapibus, risus sed tempus lacinia, tortor felis facilisis mi, nec vestibulum neque ex id mauris."
+  end.save
+end
